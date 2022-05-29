32507978e6fb5a8d640628ffddc5fa1bb9d10fd1
Marcelo Teixeira dos Santos <marcelo.teixeira@cern.ch>
2022-05-29 18:49:56 +0200

    Use checked instead of defaultChecked
    This allows for state updates to be reflected on the interface as tasks are archived and pinned.

--- src/components/Task.js
+++ src/components/Task.js
@@ -11,7 +11,7 @@ function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
       <label className="checkbox">
         <input
           type="checkbox"
-          defaultChecked={state === TASK_ARCHIVED}
+          checked={state === TASK_ARCHIVED}
           disabled={true}
           name="checked"
         />
