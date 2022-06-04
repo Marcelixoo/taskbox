3a3f7b02603f8ec503c1597598da0c71c617b286
Marcelo T. dos Santos <marcelo.teixeira@momox.biz>
2022-06-04 13:08:15 +0100

    Add interaction to showcase screen on story

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -3,9 +3,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import { rest } from 'msw';
-import { MockedState } from './TaskList.stories';
+
+import {
+  fireEvent,
+  within,
+  waitForElementToBeRemoved
+} from '@storybook/testing-library';
 
 import store, { endpoint } from '../store';
+
+import { MockedState } from './TaskList.stories';
 
 import InboxScreen from './InboxScreen';
 
@@ -16,6 +23,24 @@ export default {
   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 };
 
+const waitForTasksToLoad = async (canvas) => {
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'), {
+    timeout: 1000,
+  })
+}
+
+const pinOneTask = (canvas) => {
+  setTimeout(() => {
+    fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  }, 800);
+};
+
+const archiveOneTask = (canvas) => {
+  setTimeout(() => {
+    fireEvent.click(canvas.getByLabelText('archiveTask-3'));
+  }, 2000)
+}
+
 const Template = () => <InboxScreen />;
 
 export const Default = Template.bind({});
@@ -24,11 +49,20 @@ Default.parameters = {
     handlers: [
       rest.get(endpoint, (_, res, ctx) => {
         return res(
+          ctx.delay(800),
           ctx.json(MockedState.tasks)
         );
       }),
     ],
   },
+};
+Default.play = async({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await waitForTasksToLoad(canvas);
+
+  pinOneTask(canvas);
+  archiveOneTask(canvas);
 };
 
 export const Error = Template.bind({});
